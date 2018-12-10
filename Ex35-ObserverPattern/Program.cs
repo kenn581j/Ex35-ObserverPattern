fec02fbcbf0d274bb1ded6e51063c84385c579bf
LAPTOP-Kenneth <kennethdammortensen@gmail.com>
2018-12-10 09:09:01 +0100

    Add project files.

---
+++ Ex35-ObserverPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex35_ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
